dceee06ff756e4eee8deaf71f3a4ca3babcac230
Paul Schulze <39519149+bl4ckp4nther4@users.noreply.github.com>
2021-07-19 15:21:02 +0200

    bugfixes

--- python_script/discourse.py
+++ python_script/discourse.py
@@ -3,5 +3,6 @@ importlib.import_module('data.website_base_data')
 from data.website_base_data import WEBSITE_URL
 from data.discourse_make_dataset import MakeDataset
 
-dataset = MakeDataset(WEBSITE_URL)
-dataset()
+dataset = MakeDataset()(WEBSITE_URL, overwrite_json=True)
+
+print(dataset)
