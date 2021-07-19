1313d165705823775e9a4a12a2a71292e608ca50
Paul Schulze <39519149+bl4ckp4nther4@users.noreply.github.com>
2021-07-19 14:05:32 +0200

    converting timestamp to int

--- python_script/data/discourse_data_loader.py
+++ python_script/data/discourse_data_loader.py
@@ -16,13 +16,23 @@ class DiscourseDataLoader():
         # loads the data from json files into memory
 
         # sorting both lists by alphabet to ensure correct sequencing
-        profiles_json_list = profiles_json_list.sort()
-        post_histories_json_list = post_histories_json_list.sort()
+        profiles_json_list.sort()
+        post_histories_json_list.sort()
+
+        # data at these key need to be converted to integer
+        timestamp_keys = ['post_timestamp', 'join_timestamp', 'last_post_timestamp']
+            
 
         for profile_path in profiles_json_list:
             with open(profile_path) as jsonFile:
                 profile = json.load(jsonFile)
                 jsonFile.close()
+
+            print(profile)
+            # changing string to int
+            for timestamp_key in timestamp_keys:
+                if timestamp_key in profile:
+                    profile[timestamp_key] = int(profile[timestamp_key])
 
             self.profiles.append(profile)
 
@@ -30,6 +40,11 @@ class DiscourseDataLoader():
             with open(post_history_path) as jsonFile:
                 post_history = json.load(jsonFile)
                 jsonFile.close()
+
+            # changing string to int
+            for timestamp_key in timestamp_keys:
+                if timestamp_key in post_history:
+                    profile[timestamp_key] = int(profile[timestamp_key])
 
             self.post_histories.append(post_history)
 
