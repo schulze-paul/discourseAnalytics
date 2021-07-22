8ec08d92dd69208b7b7bba1abe3267e28adc7411
Paul Schulze <39519149+bl4ckp4nther4@users.noreply.github.com>
2021-07-22 19:20:19 +0200

    adding tests to suite

--- python_script/test/test_suite.py
+++ python_script/test/test_suite.py
@@ -6,13 +6,19 @@ from python_script.test.test_discourse_data_loader import TestDiscourseDataLoade
 
 def all_tests_suite():
     suite = unittest.TestSuite()
+    
     suite.addTest(TestDiscourseDownloader('test_get_html_from_url'))
     suite.addTest(TestDiscourseDownloader('test_download_user_list'))
     suite.addTest(TestDiscourseDownloader('test_download_user_data'))
+    suite.addTest(TestDiscourseDownloader('test_overwriting'))
 
-
-    suite.addTest(TestDiscourseConverter('test_converter'))
-    suite.addTest(TestDiscourseDataLoader('test_widget_resize'))
+    suite.addTest(TestDiscourseConverter('test_convert_user_profiles'))
+    suite.addTest(TestDiscourseConverter('test_convert_post_histories'))
+    suite.addTest(TestDiscourseConverter('test_overwriting'))
+    
+    suite.addTest(TestDiscourseDataLoader('test_timestamps_to_integer'))
+    suite.addTest(TestDiscourseDataLoader('test_call'))
+    
     return suite
 
 if __name__ == '__main__':
