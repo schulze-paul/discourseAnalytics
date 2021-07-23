a81529e3843f9d855e7f5f3c5ddf5ca82bdf5bec
Paul Schulze <39519149+bl4ckp4nther4@users.noreply.github.com>
2021-07-23 17:04:48 +0200

    moving files

--- python_script/discourse.py
+++
@@ -1,8 +0,0 @@
-import importlib
-importlib.import_module('data.website_base_data')
-from data.website_base_data import WEBSITE_URL
-from data.discourse_make_dataset import MakeDataset
-
-dataset = MakeDataset()(WEBSITE_URL, overwrite_json=True)
-
-print(dataset)
