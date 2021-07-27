059d4706523c795575af24df6b931c4bd3196ad0
Paul Schulze <39519149+bl4ckp4nther4@users.noreply.github.com>
2021-07-27 11:46:18 +0200

    adding documentation; moving int conv to converter

--- python_script/data/discourse_data_loader.py
+++ python_script/data/discourse_data_loader.py
@@ -2,66 +2,52 @@ import json
 
 class DiscourseDataLoader():
     """
-    
+    Discourse data loader class
+    Loads the data from json files
     """
     profiles = []
     post_histories = []
 
 
     def __init__(self):
-        pass
+        """
+        Set up the downloader
 
+        """
 
     def __call__(self, profiles_json_list, post_histories_json_list):
-        # loads the data from json files into memory
-
+        """
+        Loads the data from json files into memory
+        
+        Input:
+        :param profiles_json_list: list of strings, locations of the profile json files
+        :param post_histories_json_list: list of strings, locations of the post history json files
+        """
+        
         # sorting both lists
         post_histories_json_list = sorted(post_histories_json_list)
         profiles_json_list = sorted(profiles_json_list)
-
+        
+        # loading the profile files
         for profile_path in profiles_json_list:
             with open(profile_path) as jsonFile:
                 profile = json.load(jsonFile)
                 jsonFile.close()
 
-                profile = self.timestamps_to_integer(profile)
-            
             self.profiles.append(profile)
 
-
+        # loading the post history files
         for post_history_path in post_histories_json_list:
             with open(post_history_path) as jsonFile:
                 post_history = json.load(jsonFile)
                 jsonFile.close()
 
-            
-            # changing string to int
-            new_post_history = []
-            for post in post_history:
-                post = self.timestamps_to_integer(post)
-                new_post_history.append(post)
-
-            self.post_histories.append(new_post_history)
+            self.post_histories.append(post_history)
 
 
         posts = self._combine_profiles_and_post_histories( self.profiles, self.post_histories)
 
         return posts
-
-
-
-    def timestamps_to_integer(self, dict):
-        # data at these key need to be converted to integer
-        timestamp_keys = ['post_timestamp', 'join_timestamp', 'last_post_timestamp']
-
-        # changing string to int
-        updated_dict = {}
-        for timestamp_key in timestamp_keys:
-            if timestamp_key in dict:
-                updated_dict[timestamp_key] = int(dict[timestamp_key])
-        dict.update(updated_dict)
-        
-        return dict
 
 
     def _combine_profiles_and_post_histories(self, profiles, post_histories):
